a4cb4c5fdef4e961df5339a7478ac0c4638ba88f
Михаил Ефимов <mifimov345@gmail.com>
2024-03-17 19:25:29 +0500

    v1.4

--- app/src/main/java/emk/driver/gpsdriverapplication/AdminActivity.kt
+++ app/src/main/java/emk/driver/gpsdriverapplication/AdminActivity.kt
@@ -13,6 +13,8 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.content.Intent
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.ComponentActivity
@@ -29,14 +31,63 @@ import emk.driver.gpsdriverapplication.ui.theme.GPSDriverApplicationTheme
 import emk.driver.oversimplification.databinding.ActivityMainBinding
 import emk.driver.oversimplification.databinding.AdminActivityMainBinding
 import emk.driver.gpsdriverapplication.services.LocationService
+import retrofit2.Call
+import retrofit2.http.Body
+import retrofit2.http.POST
+import retrofit2.http.GET
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+
+data class CreateRequest(val login: String, val password: String , val type: String)
+
+data class CreateResponse(val answer: String)
+
+interface CreateService {
+    @POST("/create")
+    fun sendRequest(@Body request: CreateRequest): Call<CreateResponse>
+}
 
 
 class AdminActivity : AppCompatActivity() {
+
+    private val retrofit = Retrofit.Builder()
+        .baseUrl("http://10.0.2.2:5000/create/") // Replace this with your server URL
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+
+    private val createService = retrofit.create(CreateService::class.java)
+
+    private lateinit var editCreationLogin : EditText
+    private lateinit var editCreationPassword : EditText
+    private lateinit var spinner_of_type : Spinner
+    private lateinit var CreateButton : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = AdminActivityMainBinding.inflate(layoutInflater)
 
         setContentView(binding.root)
+
+        editCreationLogin = binding.editCreationLogin
+        editCreationPassword = binding.editCreationPassword
+        spinner_of_type = binding.spinnerOfType
+        CreateButton = binding.CreateButton
+
+        val items = arrayOf("User","Admin")
+
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, items)
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+        spinner_of_type.adapter = adapter
+
+        spinner_of_type.setSelection(0)
+
+        CreateButton.setOnClickListener{
+            createUser()
+        }
 
         val pointsText: TextView = binding.pointsText
         pointsText.text = LastLoginManager.pointsAmount.toString()
@@ -53,6 +104,32 @@ class AdminActivity : AppCompatActivity() {
 
         val serviceIntent = Intent(this, LocationService::class.java)
         startService(serviceIntent)
+    }
+
+    private fun createUser(){
+        val enteredUsername = editCreationLogin.text.toString()
+        val enteredPassword = editCreationPassword.text.toString()
+        val enteredType = spinner_of_type.selectedItem.toString()
+
+        val request = CreateRequest(enteredUsername,enteredPassword,enteredType)
+        createService.sendRequest(request).enqueue(object: Callback<CreateResponse>{
+            override fun onResponse(call: Call<CreateResponse>, response: Response<CreateResponse>){
+                if (response.isSuccessful){
+                    Toast.makeText(this@AdminActivity,"Успешно!", Toast.LENGTH_SHORT).show()
+                }
+                else{
+                    Toast.makeText(this@AdminActivity,"Пользователь уже существует!",Toast.LENGTH_SHORT).show()
+                }
+            }
+
+            override fun onFailure(call: Call<CreateResponse>, t: Throwable) {
+                Toast.makeText(this@AdminActivity, "Error: ${t.message}", Toast.LENGTH_SHORT).show()
+                println("Error: ${t.message}")
+                t.printStackTrace()
+            }
+        })
+
+
     }
 
     private fun logout() {
